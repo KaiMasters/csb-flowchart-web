56bc7b230f50a05aec2f039e8b36533675578134
William Peracchio <willperacchio@users.noreply.github.com>
2019-07-05 00:18:13 -0400

    Skeletoned out a bit more, noticed some logic errors

--- client/src/App.vue
+++ client/src/App.vue
@@ -95,7 +95,7 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir", 'Montserrat', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
