80cef04058d02dfcf91e7fbb09aa8262c6271625
William Peracchio <willperacchio@users.noreply.github.com>
2019-07-09 00:16:40 -0400

    Restructure aside and select component

--- client/src/components/Aside.vue
+++ client/src/components/Aside.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="aside" class="column is-one-quarter">
-    {{msg}}  
-    <div v-for="course in $props.CourseList" v-bind:key="'AsideButton_' + course.id">
-      <AsideButton :Course="course"/>
+    <select v-model="dropdownState">
+      <option>Courses</option>
+      <option>AP/Transfer Credit</option>
+    </select>
+    <div id="aside-container">
+      <div v-if="dropdownState === 'Courses'">
+        <div v-for="course in $props.CourseList" v-bind:key="'AsideButton_' + course.id">
+          <AsideButton :Course="course"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +28,7 @@ export default {
   },
   data () {
     return {
+      dropdownState: 1
     }
   },
   methods: {
@@ -33,7 +41,38 @@ export default {
 
 #aside {
   display: inline-block;
-  overflow-y: scroll;
+  padding: 0%;
+}
+
+select {
+  width: 100%;
+  height: 10%;
+  border-radius: 15px 15px 0 0;
+  color: white;
+  background-color: #C62828;
+  font-size: 1rem;
+  padding-left: 5%;
+  transition: .3s;
+  cursor: pointer;
+}
+
+select:hover {
+  background-color: #E53935;
+}
+
+select:focus {
+  outline: none;
+}
+
+option {
+  background-color: #EF9A9A;
+  color: black;
+  cursor: pointer;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, .2);
+}
+
+option:hover {
+  background-color: #FFCDD2;
 }
 
 </style>
