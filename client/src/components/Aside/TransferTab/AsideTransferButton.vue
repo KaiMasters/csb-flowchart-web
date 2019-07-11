2254ab382751a5b46390bf9ff32c65282dbd53a6
William Peracchio <willperacchio@users.noreply.github.com>
2019-07-10 22:47:38 -0400

    Better presentational aspect to transfer

--- client/src/components/Aside/TransferTab/AsideTransferButton.vue
+++ client/src/components/Aside/TransferTab/AsideTransferButton.vue
@@ -1,8 +1,22 @@
 <template>
   <div 
    :id="'TransferToggle_' + Transfer.transferid"
+   class="main"
   >
-    {{Transfer.transferid}}
+    <div class="columns is-gapless">
+      <div class="column is-two-thirds">
+        <span class="text">{{Transfer.transferid}}</span>
+      </div>
+      <div class="column is-one-third">
+        <span class="button">
+          <select name="">
+            <option v-for="(t, index) in Transfer.transfer" v-bind:key="'TransferToggle_' + Transfer.transferid + '_Option_'+ index">
+              {{t.categories}}
+            </option>
+          </select>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,5 +37,49 @@ export default {
 </script>
 
 <style scoped>
+
+div.main {
+  font-size: .75rem;
+  width: 90%;
+  height: 100%;
+  border-radius: 5px;
+  padding: 2% 5%;
+  border: 1px solid black;
+  transition: .3s;
+  margin: 0.2%;
+  background-color: #C62828;
+  color: white;
+  overflow: hidden;
+  display: inline-block;
+  text-align: left;
+  background-clip: padding-box;
+}
+
+span.text {
+  vertical-align: middle;
+  display: inline;
+}
+
+span.button {
+  vertical-align: middle;
+  width: 100%;
+  overflow: hidden;
+  margin: auto;
+  border: 0;
+  cursor: pointer;
+  padding: 0; 
+}
+
+
+.fill {
+  vertical-align: middle;
+}
+
+select {
+  width: 100%;
+  height: 100%;
+  border: 0;
+  cursor: pointer;
+}
 
 </style>
